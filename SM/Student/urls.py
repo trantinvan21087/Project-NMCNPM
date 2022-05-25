0b46065ae2db7178a6c59899c0b9587dfbf48620
tungdaoxuan <explodingww@gmail.com>
2022-05-25 16:13:54 +0700

    login,register,logout

--- SM/Student/urls.py
+++ SM/Student/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name = "home"),
     path('studentList/', views.studentList),
     path('studentProfile/', views.studentProfile),
+    path('register/', views.register, name="register"),
+    path('login/', views.loginPage, name="login"),  
+    path('logout/', views.logoutUser, name="logout"),
 ]
--- SM/Student/views.py
+++ SM/Student/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
+from .models import *
+from .form import *
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
 def home(request):
-	return render(request,"SM/dashboard.html")
+	return render(request,"Student/dashboard.html")
 
 def studentList(request):
 	return HttpResponse('studentList')
@@ -11,23 +20,58 @@ def studentList(request):
 def studentProfile(request):
 	return HttpResponse('studentProfile')
 
-def Login(request):
-	return HttpResponse('studentProfile')
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'Student/login.html', context)
+
+def register(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request,'Account was created for ' + user)
+				return redirect('login')
+		context = {'form':form }
+		return render(request, 'Student/register.html',context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 def acceptStudent(request):
-	return HttpResponse('studentProfile')
+	return HttpResponse('Login')
 
 def searchStudent(request):
-	return HttpResponse('studentProfile')
+	return HttpResponse('searchStudent')
 
 def createClass(request):
-	return HttpResponse('studentProfile')
+	return HttpResponse('createClass')
 
 def studentPoint(request):
-	return HttpResponse('studentProfile')
+	return HttpResponse('studentPoint')
 
 def teacherReport(request):
-	return HttpResponse('studentProfile')
+	return HttpResponse('teacherReport')
 
 def changeRule(request):
-	return HttpResponse('studentProfile')
+	return HttpResponse('changeRule')
